9d7617a8c8d07b7b8095a68c5281eb56c8bf5300
renseaklim <151463431+renseaklim@users.noreply.github.com>
2024-01-18 15:34:11 +0700

    1/8/2023

--- Exercise_SemesterI/src/Main.java
+++ Exercise_SemesterI/src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        TaskOne obj = new TaskOne();
+        //TaskOne obj = new TaskOne();
         //obj.exerciseFour();
-        TaskTwo obj1 = new TaskTwo();
-        TaskFour obj3 = new TaskFour();
+        //TaskTwo obj1 = new TaskTwo();
+        //TaskFour obj3 = new TaskFour();
         //obj1.VendingMachine();
-        String st = "IDKHM1603633111<<<<<<<<<<<<<<<9501016M2406215KHM<<<<<<<<<<<6MEN<<CHENDA<<<<<<<<<<<<<<<<<<<";
+        /*String st = "IDKHM1603633111<<<<<<<<<<<<<<<9501016M2406215KHM<<<<<<<<<<<6MEN<<CHENDA<<<<<<<<<<<<<<<<<<<";
         String st1 = "idNumberber:160363311";
-        obj3.Manipulate_String();
-
+        obj3.Manipulate_String(); */
+        TaskEight x = new TaskEight();
+        x.input(0);
     }
 }
